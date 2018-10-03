74b26c0dc035ba02c2791d89ee9e4e9b8cb7116e
gavinev98 <gavin.everett48@hotmail.com>
2018-10-03 20:35:34 +0100

    Added in the setters and getter methods to the Dragon Class.

--- src/Dragon.java
+++ src/Dragon.java
@@ -1,2 +1,81 @@
+import java.rmi.activation.ActivationGroup_Stub;
+import java.util.Arrays;
+
 public class Dragon {
+
+    String name = "Spyro The Dragon";
+    double health = 100;
+    String[] attackOptions = new String[]{ "Breath Fire","Tale Swing" };
+    String[] specialAbility = new String[] {"Deal 100 Damage", "Sumo"};
+    String[] clothingOptions = new String[] {"Red Hat", "Yellow Hat"};
+
+    public Dragon(String name, double health, String[] attackOptions, String[] specialAbility, String[] clothingOptions)
+    {
+        this.name = name;
+        this.health = health;
+        this.attackOptions = attackOptions;
+        this.specialAbility = specialAbility;
+        this.clothingOptions = clothingOptions;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setHealth(double health)
+    {
+        this.health = health;
+    }
+
+    public void setAttackOptions(String[] attackOptions)
+    {
+        this.attackOptions = attackOptions;
+    }
+
+    public void setSpecialAbility(String[] specialAbility)
+    {
+        this.specialAbility = specialAbility;
+    }
+
+    public void setClothingOptions(String[] clothingOptions)
+    {
+        this.clothingOptions = clothingOptions;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getHealth()
+    {
+        return health;
+    }
+
+    public String[] getAttackOptions()
+    {
+        return attackOptions;
+    }
+
+    public String[] getSpecialAbility()
+    {
+        return specialAbility;
+    }
+
+    public String[] getClothingOptions()
+    {
+        return clothingOptions;
+    }
+
+    public String toString()
+    {
+        return "Name" + getName() + "Health" + getHealth() + "Attack Options" + Arrays.toString(attackOptions) + "Special Ability" + Arrays.toString(specialAbility) + "Clothing Options" + Arrays.toString(clothingOptions);
+    }
+
+
+
+
+
+
 }
