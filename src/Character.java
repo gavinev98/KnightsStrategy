2d111bfa3be27ec9dbe2ec779c7dafd0ccb9b680
gavinev98 <gavin.everett48@hotmail.com>
2018-10-03 21:47:30 +0100

    Added in the setters and getter methods to the Character Class.

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,77 @@
+import java.util.Arrays;
+
 public class Character {
+
+
+
+        String name;
+        double health;
+        String[] attackOptions;
+        String[] specialAbility;
+        String[] clothingOptions;
+
+        public Character(String name, double health, String[] attackOptions, String[] specialAbility, String[] clothingOptions)
+        {
+            this.name = name;
+            this.health = health;
+            this.attackOptions = attackOptions;
+            this.specialAbility = specialAbility;
+            this.clothingOptions = clothingOptions;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setHealth(double health)
+        {
+            this.health = health;
+        }
+
+        public void setAttackOptions(String[] attackOptions)
+        {
+            this.attackOptions = attackOptions;
+        }
+
+        public void setSpecialAbility(String[] specialAbility)
+        {
+            this.specialAbility = specialAbility;
+        }
+
+        public void setClothingOptions(String[] clothingOptions)
+        {
+            this.clothingOptions = clothingOptions;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public double getHealth()
+        {
+            return health;
+        }
+
+        public String[] getAttackOptions()
+        {
+            return attackOptions;
+        }
+
+        public String[] getSpecialAbility()
+        {
+            return specialAbility;
+        }
+
+        public String[] getClothingOptions()
+        {
+            return clothingOptions;
+        }
+
+        public String toString()
+        {
+            return "Name" + getName() + "Health" + getHealth() + "Attack Options" + Arrays.toString(attackOptions) + "Special Ability" + Arrays.toString(specialAbility) + "Clothing Options" + Arrays.toString(clothingOptions);
+        }
 
 }
