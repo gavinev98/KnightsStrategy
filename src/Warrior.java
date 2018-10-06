11f73c8173661f9c92c781b0165a8c84471e6c34
gavinev98 <gavin.everett48@hotmail.com>
2018-10-06 19:51:39 +0100

    Removed setter and getter methods to improve cohesiveness and abide by the single responsibility principle.

--- src/Warrior.java
+++ src/Warrior.java
@@ -8,35 +8,6 @@ public class Warrior extends Character implements FightingOptions, IsAlive {
     }
 
 
-    public String getName()
-    {
-        return name;
-    }
-
-    public double getHealth()
-    {
-        return health;
-    }
-
-    public String[] getAttackOptions()
-    {
-        return attackOptions;
-    }
-
-    public String[] getSpecialAbility()
-    {
-        return specialAbility;
-    }
-
-    public String[] getClothingOptions()
-    {
-        return clothingOptions;
-    }
-
-    public String toString()
-    {
-        return "Name" + getName() + "Health" + getHealth() + "Attack Options" + Arrays.toString(attackOptions) + "Special Ability" + Arrays.toString(specialAbility) + "Clothing Options" + Arrays.toString(clothingOptions);
-    }
 
     @Override
     public double attack() {
