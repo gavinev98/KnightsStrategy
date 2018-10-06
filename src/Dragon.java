bd1418a4617fdfbd4753c47ff9d4a57eee2e9634
gavinev98 <gavin.everett48@hotmail.com>
2018-10-06 19:51:12 +0100

    Removed setter and getter methods to improve cohesiveness and abide by the single responsibility principle.

--- src/Dragon.java
+++ src/Dragon.java
@@ -8,36 +8,6 @@ public class Dragon extends Character implements FightingOptions, IsAlive {
         super(name, health, attackOptions, specialAbility, clothingOptions);
     }
 
-    public String getName()
-    {
-        return name;
-    }
-
-    public double getHealth()
-    {
-        return health;
-    }
-
-    public String[] getAttackOptions()
-    {
-        return attackOptions;
-    }
-
-    public String[] getSpecialAbility()
-    {
-        return specialAbility;
-    }
-
-    public String[] getClothingOptions()
-    {
-        return clothingOptions;
-    }
-
-    public String toString()
-    {
-        return "Name" + getName() + "Health" + getHealth() + "Attack Options" + Arrays.toString(attackOptions) + "Special Ability" + Arrays.toString(specialAbility) + "Clothing Options" + Arrays.toString(clothingOptions);
-    }
-
 
     @Override
     public double attack() {
