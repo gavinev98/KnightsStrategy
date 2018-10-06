4675788609f5ba93493003848256cd4a85227679
gavinev98 <gavin.everett48@hotmail.com>
2018-10-06 15:22:09 +0100

    Added another interface called IsAlive.

--- src/Wizard.java
+++ src/Wizard.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Wizard extends Character implements FightingOptions {
+public class Wizard extends Character implements FightingOptions, IsAlive {
 
     public Wizard(String name, double health, String[] attackOptions, String[] specialAbility, String[] clothingOptions) {
         super(name, health, attackOptions, specialAbility, clothingOptions);
