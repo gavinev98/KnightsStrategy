46d465cc7c6f5af754e49b9f13b10c7d26a6ac1f
gavinev98 <gavin.everett48@hotmail.com>
2018-10-06 19:51:32 +0100

    Removed setter and getter methods to improve cohesiveness and abide by the single responsibility principle.

--- src/Troll.java
+++ src/Troll.java
@@ -7,35 +7,6 @@ public class Troll extends Character implements FightingOptions, IsAlive {
         super(name, health, attackOptions, specialAbility, clothingOptions);
     }
 
-    public String getName()
-    {
-        return name;
-    }
-
-    public double getHealth()
-    {
-        return health;
-    }
-
-    public String[] getAttackOptions()
-    {
-        return attackOptions;
-    }
-
-    public String[] getSpecialAbility()
-    {
-        return specialAbility;
-    }
-
-    public String[] getClothingOptions()
-    {
-        return clothingOptions;
-    }
-
-    public String toString()
-    {
-        return "Name" + getName() + "Health" + getHealth() + "Attack Options" + Arrays.toString(attackOptions) + "Special Ability" + Arrays.toString(specialAbility) + "Clothing Options" + Arrays.toString(clothingOptions);
-    }
 
 
     @Override
