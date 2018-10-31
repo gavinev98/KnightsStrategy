8980303ad04a0c29d50212b83150f5e20f2cfc0e
gavinev98 <gavin.everett48@hotmail.com>
2018-10-31 12:56:36 +0000

    Added subject interface.

---
+++ src/Subject.java
@@ -0,0 +1,10 @@
+import java.util.Observer;
+
+public interface Subject {
+        //Register observer
+        public void register(Observer o);
+        // Unregister observer
+        public void unregister(Observer o);
+        // Notfiy observers
+        public void notifyObserver();
+}
