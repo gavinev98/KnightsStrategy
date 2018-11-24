a21c14adc2dff37bbbca1fcfad4e9603144f2091
gavinev98 <gavin.everett48@hotmail.com>
2018-11-24 22:51:31 +0000

    Added composition for fighting options interface.

--- src/Troll.java
+++ src/Troll.java
@@ -2,9 +2,12 @@ import TemplateMethod.LootMethod;
 
 public class Troll extends Character implements CharacterClothing,  IsAlive {
 
+    //Using Composition
+    FightingOptions fightingOptions;
 
     public Troll(String name, double health, String attackOptions, String specialAbility, String clothingOptions, LootMethod method) {
         super(name, health, attackOptions, specialAbility, clothingOptions, method);
+        fightingOptions = new TrollSword();
     }
 
 
