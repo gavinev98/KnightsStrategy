08f1895402dcba885019d466966b3dacfad514d5
gavinev98 <gavin.everett48@hotmail.com>
2018-11-25 20:34:32 +0000

    Acidentally removed subject class.

--- src/Subject.java
+++ src/Subject.java
@@ -1,9 +1,9 @@
 
 public interface Subject {
-        //Register observer
-        public void register(Observer o);
-        // Unregister observer
-        public void unregister(Observer o);
-        // Notfiy observers
-        public void notifyObserver();
+    //Register observer
+    public void register(Observer o);
+    // Unregister observer
+    public void unregister(Observer o);
+    // Notfiy observers
+    public void notifyObserver();
 }
