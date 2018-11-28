b8ef0fb61a63e03a7733023bf2097ef4af9fb452
gavinev98 <gavin.everett48@hotmail.com>
2018-11-28 21:25:36 +0000

    Added state to character class.

--- src/Models/Troll.java
+++ src/Models/Troll.java
@@ -2,20 +2,19 @@ package Models;
 
 import Decorator.CharacterClothing;
 import Models.Character;
+import State.*;
 import Strategy.TrollSword;
 import TemplateMethod.LootMethod;
 import Interface.*;
 
 public class Troll extends Character implements CharacterClothing, IsAlive {
 
-
-
     public Troll(String name, double health, String attackOptions, String clothingOptions, LootMethod method) {
         super(name, health, attackOptions, clothingOptions, method);
         fightingOptions = new TrollSword();
+
+
     }
-
-
 
 
     @Override
